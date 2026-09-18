0e31060e99505405ac1543a2a958787003892bbc
Aaron Scherer <aequasi@gmail.com>
2026-09-18 11:30:42 -0500

    test(emit): stop measuring allocations where the counter is process-wide

--- tests/Concord.Emit.Tests/TestPolyfills.cs
+++ tests/Concord.Emit.Tests/TestPolyfills.cs
@@ -22,25 +22,11 @@ internal sealed class SkipOnMonoFactAttribute : FactAttribute {
 }
 
 internal static class TestPolyfills {
-#if !NET
-    private static readonly bool IsMono = Type.GetType("Mono.Runtime") is not null;
-
-    static TestPolyfills() {
-        if (!IsMono) {
-            AppDomain.MonitoringIsEnabled = true;
-        }
-    }
-#endif
-
     public static long GetAllocatedBytes() {
 #if NET
         return GC.GetAllocatedBytesForCurrentThread();
 #else
-        if (IsMono) {
-            return 0L;
-        }
-
-        return AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize;
+        return 0L;
 #endif
     }
 
